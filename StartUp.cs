bcf40c9fbb78112641c5fbcd6e28c889d6195c28
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-03-03 11:14:43 +0100

    1st

---
+++ StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+            randomList.Add("Pesho");
+            randomList.Add("Dimitrichko");
+            randomList.Add("Gogi");
+            randomList.Add("Manitu");
+            randomList.Add("Gesho");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(randomList.RandomString());
+            }
+        }
+    }
+}
